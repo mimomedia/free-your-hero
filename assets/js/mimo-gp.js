76679b3d72b8ac7b7f8d389b7efdd5ab6748faef
mimo <mimocontact@gmail.com>
2017-05-22 01:22:48 +0200

    Show progress bar on Start Again

--- assets/js/mimo-gp.js
+++ assets/js/mimo-gp.js
@@ -21,6 +21,7 @@ function mimoInit() {
     }
     // If any of the questions if answered
     if (results.length > 0) {
+
     	// Get the most repeated result in results array
         var repeated = {};
         var max = 0;
@@ -37,18 +38,21 @@ function mimoInit() {
             percentage = questions * 100 / count;
         // Update progress bar
 		document.getElementById('mimo-progress-bar').style.width = percentage + '%';
-		var bottomResult = document.getElementById('mimo-bottom');
-        var bottomResultDiv = document.getElementById('mimo-result-bottom');
+		var bottomResult = document.getElementById('mimo-bottom'),
+        	bottomResultDiv = document.getElementById('mimo-result-bottom'),
+			progressDiv = document.getElementById('mimo-progress');
         
-        if (percentage === 100) {
+        if ( 100 === percentage ) {
         	// The quiz is finished
-            document.getElementById('mimo-progress').style.display = 'none';
+            progressDiv.style.display = 'none';
             // Show bottom results
             bottomResult.style.display = 'block';	
             bottomResultDiv.innerHTML = '<h2 class="mimo-activist">' + mimoResult(result) + '</h2><p class="mimo-lead">Congratulations, you ended the research, we found that the hero inside you is a <strong>' + mimoResult(result) + '</strong>, so you can go the next step and free him!</p>';
 
         } else {
         	// The quiz is not finished 
+        	progressDiv.style.display = 'block';
+        	progressDiv.style.display = 'block';
         	// Show bottom results 
             bottomResult.style.display = 'block';
             bottomResultDiv.innerHTML = '<p class="mimo-lead">You have completed ' + percentage + '% of the challenge. By now you seem to have a <strong>' + mimoResult(result) + '</strong> inside.</p><p>Finish the quiz to find your inside living <strong>Hero</strong> or take action right now.</p>';
@@ -101,10 +105,6 @@ function mimoRestart() {
     for (var i = 0; i < input.length; i++) input[i].checked = false;
     // Hide bottom results
     document.getElementById('mimo-bottom').style.display = 'none';
-    // Hide progress bar
-	document.getElementById('mimo-progress').style.display = 'none';
-	// Initialize progress bar
-    document.getElementById('mimo-progress-bar').style.width = '0';
     // Remove answered classes
     var container = document.getElementsByClassName('mimo-question');
     for (i = 0; i < container.length; ++i) { container[i].classList.remove('mimo-answered') };
